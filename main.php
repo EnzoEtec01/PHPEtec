04b45a1f2dfbd85a0b21a34abf3851d7b0981a79
EnzoEtec01 <161708585+EnzoEtec01@users.noreply.github.com>
2024-03-07 08:05:17 -0300

    Add files via upload

--- main.php
+++ main.php
@@ -1,33 +1,61 @@
 <?php
+/*
+    Nome: Enzo de Campos Machado
+    Prof: Rafael SW-Sistemas Web
+    Data: 07-03-2024
+*/
 
-    class Fruta{
+    class Carro{
+        public $modelo;
         public $nome;
+        public $montadora;
         public $cor;
+        public $ano;
+        
+        //funçao liga e desliga//
+        
+        function liga_carro(){
+            return "o carro esta ligado";
+        }
+    
+        function desliga_carro(){
+             return "o carro esta desligado";
+        }
     }
+
+
+    $Carro1 = new Carro();
+    $Carro1->modelo = "SUV";
+    $Carro1->nome = "Tracker";
+    $Carro1->montadora = "Chevrolet";
+    $Carro1->cor = "VERDE";
+    $Carro1->ano = "2020";
     
-    $banana = new Fruta();
-    $banana->nome = "Banana";
-    $banana->cor = "Amarela";
+    $Carro2 = new Carro();
+    $Carro2->modelo = "Ford";
+    $Carro2->nome = "Maverick";
+    $Carro2->montadora = "Ford";
+    $Carro2->cor = "Preto";
+    $Carro2->ano = "2020";
+
+   /* //carro 1
+    print $Carro1->modelo . "\n";
+    print $Carro1->nome . "\n";
+    print $Carro1->montadora . "\n";
+    print $Carro1->cor . "\n";
+    print $Carro1->ano . "\n\n";
+    //carro 2
+    print $Carro2->modelo . "\n";
+    print $Carro2->nome . "\n";
+    print $Carro2->montadora . "\n";
+    print $Carro2->cor . "\n";
+    print $Carro2->ano . "\n\n";*/
     
-    $morango = new Fruta();
-    $morango->nome = "Morango";
-    $morango->cor = "Vermelho";
+    print $Carro1->liga_carro() . "\n";
+    print $Carro1->desliga_carro() . "\n";
+    print $Carro2->liga_carro() . "\n";
+    print $Carro2->desliga_carro();
     
-    $cereja = new Fruta();
-    $cereja->nome = "Cereja";
-    $cereja->cor = "Vermelho";
     
-    $pitanga = new Fruta();
-    $pitanga->nome = "Pitanga";
-    $pitanga->cor = "Laranja";
-    
-    $abacaxi = new Fruta();
-    $abacaxi->nome = "Abacaxi";
-    $abacaxi->cor = "Amarelo e Verde";
-    
-    print $banana->cor . "\n";
-    print $morango->cor . "\n";
-    print $cereja->cor . "\n";
-    print $pitanga->cor . "\n";
-    print $abacaxi->cor . "\n";
+
 ?>
